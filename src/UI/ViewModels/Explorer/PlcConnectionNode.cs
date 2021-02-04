ceb9020b6bb257e1fc907c8e574e2636204c50d8
Michael Croes <mycroes@gmail.com>
2021-02-04 23:23:00 +0100

    Add variables, fake read to PlcConnectionNode

--- src/UI/ViewModels/Explorer/PlcConnectionNode.cs
+++ src/UI/ViewModels/Explorer/PlcConnectionNode.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Reactive;
+using System.Threading.Tasks;
+using DynamicData.Binding;
 using PlcMonitor.UI.Models;
+using PlcMonitor.UI.Models.PlcData;
 using ReactiveUI;
 
 namespace PlcMonitor.UI.ViewModels.Explorer
@@ -7,17 +11,36 @@ namespace PlcMonitor.UI.ViewModels.Explorer
     public class PlcConnectionNode : IExplorerNode
     {
         private readonly ProjectViewModel _project;
-        private readonly IPlc _plc;
-        public string Name => _plc.Name;
+        public IPlc Plc { get; }
+
+        public string Name => Plc.Name;
 
         public ReactiveCommand<Unit, Unit> DeleteCommand { get; }
+
+        public ReactiveCommand<Unit, Unit> ReadCommand { get; }
+
+        public ObservableCollectionExtended<VariableViewModel> Variables { get; } = new();
 
         public PlcConnectionNode(ProjectViewModel project, IPlc plc)
         {
             _project = project;
-            _plc = plc;
+            Plc = plc;
 
-            DeleteCommand = ReactiveCommand.Create(() => {project.Plcs.Remove(plc); });
+            DeleteCommand = ReactiveCommand.Create(() => { project.Plcs.Remove(plc); });
+            ReadCommand = ReactiveCommand.CreateFromTask(Read);
+
+            Variables.Add(new VariableViewModel { Address = "DB1,INT2" });
+            Variables.Add(new VariableViewModel { Address = "DB1,INT5" });
+        }
+
+        private Task Read()
+        {
+            foreach (var variable in Variables)
+            {
+                variable.Value.OnNext(new ReceivedValue(new Random().Next(), DateTimeOffset.Now));
+            }
+
+            return Task.CompletedTask;
         }
     }
 }
