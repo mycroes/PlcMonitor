bec517a15fd32fa38b9866fb61a8a5667f9f6b2e
Michael Croes <mycroes@gmail.com>
2021-02-05 22:30:02 +0100

    StorageService: Use JsonSerializerOptions

--- src/UI/Services/StorageService.cs
+++ src/UI/Services/StorageService.cs
@@ -8,12 +8,19 @@ namespace PlcMonitor.UI.Services
 {
     public class StorageService : IStorageService
     {
+        private readonly JsonSerializerOptions _options;
+
+        public StorageService(JsonSerializerOptions options)
+        {
+            _options = options;
+        }
+
         public async Task Save(Project project, string fileName)
         {
             var file = new FileInfo(fileName);
             using var stream = file.Open(FileMode.OpenOrCreate, FileAccess.Write);
 
-            await JsonSerializer.SerializeAsync(stream, project);
+            await JsonSerializer.SerializeAsync(stream, project, _options);
         }
 
         public async Task<Project> Load(string fileName)
@@ -21,7 +28,7 @@ namespace PlcMonitor.UI.Services
             var file = new FileInfo(fileName);
             using var stream = file.OpenRead();
 
-            return await JsonSerializer.DeserializeAsync<Project>(stream) ??
+            return await JsonSerializer.DeserializeAsync<Project>(stream, _options) ??
                 throw new Exception($"Failed to deserialize project from {fileName}.");
         }
     }
