bc329e983b651768090452d794aed9ade240267d
Michael Croes <mycroes@gmail.com>
2021-02-12 22:29:27 +0100

    PlcViewModel: Cleanup usings

--- src/UI/ViewModels/PlcViewModel.cs
+++ src/UI/ViewModels/PlcViewModel.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Reactive;
 using System.Threading.Tasks;
 using DynamicData.Binding;
 using PlcMonitor.UI.Models;
-using PlcMonitor.UI.Models.PlcData;
 using ReactiveUI;
 
 namespace PlcMonitor.UI.ViewModels
