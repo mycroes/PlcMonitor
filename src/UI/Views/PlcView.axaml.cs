97cf85b7429f65ccaafcfcb46c46b4aab7121e34
Michael Croes <mycroes@gmail.com>
2021-02-19 22:57:23 +0100

    Use ComboBox for ModbusVariable ObjectType and TypeCode

--- src/UI/Views/PlcView.axaml.cs
+++ src/UI/Views/PlcView.axaml.cs
@@ -7,7 +7,9 @@ using Avalonia.Data;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using DynamicData;
+using PlcMonitor.UI.Controls;
 using PlcMonitor.UI.Models;
+using PlcMonitor.UI.Models.Modbus;
 using PlcMonitor.UI.ViewModels;
 using ReactiveUI;
 
@@ -55,15 +57,50 @@ namespace PlcMonitor.UI.Views
             {
                 case S7Plc _:
                     yield return Col("Address", nameof(S7VariableViewModel.Address));
+                    yield return Col("Length", nameof(VariableViewModel.Length));
+                    yield return Col("Type", nameof(VariableViewModel.TypeCode), true);
                     break;
                 case ModbusPlc _:
-                    yield return Col("Object type", nameof(ModbusVariableViewModel.ObjectType));
+                    yield return new DataGridComboBoxColumn
+                    {
+                        Header = "Object type",
+                        Binding = new Binding(nameof(ModbusVariableViewModel.ObjectType)),
+                        Width = new DataGridLength(1, DataGridLengthUnitType.Auto),
+                        IsReadOnly = false,
+                        Items = new[] {
+                            ObjectType.Coil,
+                            ObjectType.DiscreteInput,
+                            ObjectType.HoldingRegister,
+                            ObjectType.InputRegister
+                        }
+                    };
                     yield return Col("Address", nameof(ModbusVariableViewModel.Address));
+                    yield return Col("Length", nameof(VariableViewModel.Length));
+                    // Addressing is at 16 bit register level only, so we allow manual typecode selection
+                    yield return new DataGridComboBoxColumn
+                    {
+                        Header = "Type",
+                        Binding = new Binding(nameof(VariableViewModel.TypeCode)),
+                        Width = new DataGridLength(1, DataGridLengthUnitType.Auto),
+                        IsReadOnly = false,
+                        Items = new[] {
+                            TypeCode.Boolean,
+                            TypeCode.Byte,
+                            TypeCode.Double,
+                            TypeCode.Int16,
+                            TypeCode.Int32,
+                            TypeCode.Int64,
+                            TypeCode.SByte,
+                            TypeCode.Single,
+                            TypeCode.String,
+                            TypeCode.UInt16,
+                            TypeCode.UInt32,
+                            TypeCode.UInt64
+                        }
+                    };
                     break;
             }
 
-            yield return Col("Length", nameof(VariableViewModel.Length));
-            yield return Col("Type", nameof(VariableViewModel.TypeCode), true);
             yield return Col("Value", $"{nameof(VariableViewModel.State)}.{nameof(VariableViewModel.State.Value)}", true);
             yield return Col("Last change", $"{nameof(VariableViewModel.State)}.{nameof(VariableViewModel.State.LastChange)}", true);
             yield return Col("Last read", $"{nameof(VariableViewModel.State)}.{nameof(VariableViewModel.State.LastRead)}^");
