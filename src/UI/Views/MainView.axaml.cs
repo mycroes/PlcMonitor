e0f211934ae4fec5e0a1779df6cb4cf49399696e
Michael Croes <mycroes@gmail.com>
2021-02-23 21:44:26 +0100

    MainView: Add more information to title

--- src/UI/Views/MainView.axaml.cs
+++ src/UI/Views/MainView.axaml.cs
@@ -1,4 +1,7 @@
+using System.IO;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using PlcMonitor.UI.ViewModels;
@@ -8,16 +11,35 @@ namespace PlcMonitor.UI.Views
 {
     public class MainView : ReactiveUserControl<MainWindowViewModel>
     {
+        private readonly TextBlock _title;
+
         public MainView()
         {
-            this.WhenActivated(disposables => Disposable.Create(() => { }).DisposeWith(disposables));
-
             InitializeComponent();
+
+            _title = this.FindControl<TextBlock>(nameof(_title));
+
+            this.WhenActivated(disposables => {
+                this.ViewModel.HasChanges.CombineLatest(this.ViewModel.WhenAnyValue(vm => vm.Project.File), CreateTitle).BindTo(this, v => v._title.Text).DisposeWith(disposables);
+            });
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private string CreateTitle(bool changes, FileInfo? file)
+        {
+            var res = changes ? "* " : "";
+
+            var title = file == null
+                ? "Untitled"
+                : file.Name.EndsWith(".plcson")
+                    ? file.Name.Substring(0, file.Name.Length - 7)
+                    : file.Name;
+
+            return res + title + " - PlcMonitor";
         }
     }
 }
