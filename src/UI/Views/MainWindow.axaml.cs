0a39341806456487ebf87e08c19f9bed726c2fbe
Michael Croes <mycroes@gmail.com>
2021-03-01 22:09:27 +0100

    Set window title base don current project

--- src/UI/Views/MainWindow.axaml.cs
+++ src/UI/Views/MainWindow.axaml.cs
@@ -1,11 +1,11 @@
 using System;
-using System.Reactive.Disposables;
+using System.IO;
+using System.Reactive.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml;
 using Avalonia.Platform;
-using Avalonia.ReactiveUI;
 using PlcMonitor.UI.ViewModels;
 using ReactiveUI;
 
@@ -34,6 +34,14 @@ namespace PlcMonitor.UI.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
+
+            this.WhenAnyValue(x => x.DataContext)
+                .Select(x => x as MainWindowViewModel)
+                .Where(x => x != null)
+                .Select(x => x!)
+                .SelectMany(x => x.WhenAnyValue(vm => vm.Project.File))
+                .Select(CreateTitle)
+                .BindTo(this, v => v.Title);
         }
 
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
@@ -42,6 +50,17 @@ namespace PlcMonitor.UI.Views
             ExtendClientAreaChromeHints =
                 ExtendClientAreaChromeHints.PreferSystemChrome |
                 ExtendClientAreaChromeHints.OSXThickTitleBar;
+        }
+
+        private string CreateTitle(FileInfo? file)
+        {
+            var fn = file == null
+                ? null
+                : file.Name.EndsWith(".plcson")
+                    ? file.Name.Substring(0, file.Name.Length - 7)
+                    : file.Name;
+
+            return fn == null ? "PlcMonitor" : $"{fn} - PlcMonitor";
         }
 
         private void InitializeComponent()
